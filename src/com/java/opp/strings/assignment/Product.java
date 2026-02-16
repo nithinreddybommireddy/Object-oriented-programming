c3107b2919d88007a31e7ea5ede02bba8acad377
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-02-16 20:36:19 +0530

    feat: Product class using Java Bean model

---
+++ src/com/java/opp/strings/assignment/Product.java
@@ -0,0 +1,68 @@
+package com.java.opp.strings.assignment;
+
+import java.util.Objects;
+
+public class Product {
+    private String id;
+    private String name;
+    private double price;
+    private float discountPercentage;
+
+     public Product() {
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public float getDiscountPercentage() {
+        return discountPercentage;
+    }
+
+    public void setDiscountPercentage(float discountPercentage) {
+        this.discountPercentage = discountPercentage;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Double.compare(price, product.price) == 0 && Float.compare(discountPercentage, product.discountPercentage) == 0 && Objects.equals(id, product.id) && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price, discountPercentage);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", discountPercentage=" + discountPercentage +
+                '}';
+    }
+}
