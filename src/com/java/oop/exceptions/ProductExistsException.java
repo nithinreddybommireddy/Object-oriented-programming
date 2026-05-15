bd8c565586dac44b6f56aa6b6d7c456bf377b2bb
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-05-15 16:32:56 +0530

    feat: Exception handling

---
+++ src/com/java/oop/exceptions/ProductExistsException.java
@@ -0,0 +1,9 @@
+package com.java.oop.exceptions;
+
+public  class ProductExistsException extends Exception {
+    public ProductExistsException(String message) {
+        super(message);
+
+    }
+
+}
